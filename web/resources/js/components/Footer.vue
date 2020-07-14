dddecb0f95ef4237de6da22066f1ead3fc35985d
mrmtsu <81202muramatsu@gmail.com>
2020-07-15 03:39:17 +0900

    認証ページ

---
+++ web/resources/js/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">
+    <button class="button button--link">Logout</button>
+    <RouterLink class="button button--link" to="/login">
+      Login / Register
+    </RouterLink>
+  </footer>
+</template>
+
+<style>
+.footer {
+  margin: 0 auto;
+  width: 1100px;
+}
+</style>
