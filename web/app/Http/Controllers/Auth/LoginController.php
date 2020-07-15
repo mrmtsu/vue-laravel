4ac6a57af2373e5e6005f16418ecc7a488983794
mrmtsu <81202muramatsu@gmail.com>
2020-07-15 15:58:40 +0900

    Vuex導入

--- web/app/Http/Controllers/Auth/LoginController.php
+++ web/app/Http/Controllers/Auth/LoginController.php
@@ -39,7 +39,7 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function authenticated(Request $request, $user)
+    protected function authenticated(Request $request, $user)
     {
         return $user;
     }
