0e5a37ba63aae8c04993ef2ec3c14d8d9ab52c3a
mrmtsu <81202muramatsu@gmail.com>
2020-07-17 17:50:07 +0900

    いいね機能実装

--- web/app/Photo.php
+++ web/app/Photo.php
@@ -3,32 +3,34 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Arr;
 
 class Photo extends Model
 {
     /** プライマリキーの型 */
     protected $keyType = 'string';
 
+    /** IDの桁数 */
+    const ID_LENGTH = 12;
+
+    /** JSONに含めるアクセサ */
+    protected $appends = [
+        'url', 'likes_count', 'liked_by_user',
+    ];
+
     /** JSONに含める属性 */
     protected $visible = [
         'id', 'owner', 'url', 'comments',
+        'likes_count', 'liked_by_user',
     ];
-
-    /** JSONに含める属性 */
-    protected $appends = [
-        'url',
-    ];
-
-    /** IDの桁数 */
-    const ID_LENGTH = 12;
 
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
 
-        if (! Arr::get($this->attributes, 'id')) {
+        if (! array_get($this->attributes, 'id')) {
             $this->setId();
         }
     }
@@ -64,21 +66,43 @@ class Photo extends Model
     }
 
     /**
-     * リレーションシップ - usersテーブル
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function owner()
-    {
-        return $this->belongsTo('App\User', 'user_id', 'id', 'users');
-    }
-
-    /**
      * アクセサ - url
      * @return string
      */
     public function getUrlAttribute()
     {
         return Storage::cloud()->url($this->attributes['filename']);
+    }
+
+    /**
+     * アクセサ - likes_count
+     * @return int
+     */
+    public function getLikesCountAttribute()
+    {
+        return $this->likes->count();
+    }
+
+    /**
+     * アクセサ - liked_by_user
+     * @return boolean
+     */
+    public function getLikedByUserAttribute()
+    {
+    if (Auth::guest()) {
+    return false;
+    }
+
+    return $this->likes->contains(Auth::user());
+    }
+
+    /**
+     * リレーションシップ - usersテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function owner()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id', 'users');
     }
 
     /**
@@ -90,5 +114,12 @@ class Photo extends Model
         return $this->hasMany('App\Comment')->orderBy('id', 'desc');
     }
 
-    protected $perPage = 15;
+    /**
+     * リレーションシップ - usersテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function likes()
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
 }
--- web/routes/api.php
+++ web/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 
 // 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
@@ -12,7 +11,16 @@ Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 // ログインユーザー
-Route::get('/user', fn() => Auth::user())->name('user');
+Route::get('/user', function () {
+    return Auth::user();
+})->name('user');
+
+// トークンリフレッシュ
+Route::get('/reflesh-token', function (Illuminate\Http\Request $request) {
+    $request->session()->regenerateToken();
+
+    return response()->json();
+});
 
 // 写真投稿
 Route::post('/photos', 'PhotoController@create')->name('photo.create');
@@ -25,3 +33,9 @@ Route::get('/photos/{id}', 'PhotoController@show')->name('photo.show');
 
 // コメント
 Route::post('/photos/{photo}/comments', 'PhotoController@addComment')->name('photo.comment');
+
+// いいね
+Route::put('/photos/{id}/like', 'PhotoController@like')->name('photo.like');
+
+// いいね解除
+Route::delete('/photos/{id}/like', 'PhotoController@unlike');
